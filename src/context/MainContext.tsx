7eb77b655bf8fc1d2366cdaf2f8f4ad0130c608e
alandysson <alandyssom@gmail.com>
2021-05-30 19:43:31 -0300

    fix: removendo items duplicado

--- src/context/MainContext.tsx
+++ src/context/MainContext.tsx
@@ -33,24 +33,6 @@ export const MainProvider = ({ children }: MainContextProviderProps) => {
 
    const totalItems = [...item];
 
-   function removeDuplicado() {
-      for (var i = 0; i <= totalItems.length; i++) {
-         var indices = [];
-         var elemento = totalItems[i];
-         var index = totalItems.indexOf(elemento);
-         while (index != -1) {
-            indices.push(index);
-            index = totalItems.indexOf(elemento, index + 1);
-         }
-
-         if (indices.length > 1) {
-            totalItems.splice(totalItems.indexOf(totalItems[i]), 1);
-         }
-      }
-   }
-
-   removeDuplicado();
-
    return (
       <MainContext.Provider value={{ cart, totalProduct, totalItems }}>
          {children}

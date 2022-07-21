8c33161e68437cc6850e0f51a24cf4e59524c381
Alandysson Rolim <alandyssom@gmail.com>
2022-07-21 11:22:25 -0300

    Refactor(Pagination): add next/link
    removed packages locks from application

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Pagination } from "react-bootstrap";
 import styles from "./pagination.module.scss"
 
@@ -5,21 +6,46 @@ export const CustomPagination = ({ props }) => {
    const { currentPage, totalPages } = props
    return (
       <div className={styles.paginationContainer}>
-         <a href={currentPage !== 1 ? "/1" : ""}>{"<"}</a>
+         <ButtonPagination
+            href={currentPage !== 1 ? "/1" : ""}
+            title={"<"}
+            disabled={currentPage === 1}
+         />
          {currentPage !== 1 &&
-            <a href={`/${currentPage - 1}`}>
-               {currentPage - 1}
-            </a>
+            <ButtonPagination
+               href={`/${currentPage - 1}`}
+               title={currentPage - 1}
+            />
          }
-         <a href={`/${currentPage}`}
-            className={styles.active}
-         >
-            {currentPage}
-         </a>
+         <ButtonPagination
+            href={`/${currentPage}`}
+            title={currentPage}
+            style={styles.active}
+         />
          {currentPage !== totalPages &&
-            <a href={`/${currentPage + 1}`}>{currentPage + 1}</a>
+            <ButtonPagination
+               href={`/${currentPage + 1}`}
+               title={currentPage + 1}
+            />
          }
-         <a href={`/${totalPages}`}>{">"}</a>
+         <ButtonPagination
+            href={`/${totalPages}`}
+            title={">"}
+            disabled={currentPage === totalPages}
+         />
       </div>
+   )
+}
+
+const ButtonPagination = (props) => {
+   return (
+      <Link href={props.href}>
+         <button
+            disabled={props.disabled}
+            className={props.style}
+         >
+            {props.title}
+         </button>
+      </Link>
    )
 }
